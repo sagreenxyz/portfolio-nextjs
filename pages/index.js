4a0391eae67cff87af477e8594a0d9d9c8a76dd2
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-28 18:39:07 -0500

    index page setup with content; needs button click handler

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className="text-3xl">
-      Hello, World!
+    <div className="h-screen grid grid-cols-1">
+      <div class="place-self-center">
+        <div className="text-3xl py-6">Introducing...</div>
+        <div className="text-5xl md:text-7xl lg:text-8xl font-bold text-zinc-600">SAGreenXYZ</div>
+        <div className="text-center py-6">a.k.a. Scott A. Green</div>
+        <div class="text-center py-10"><button onclick="clickHandler()" class="shadow-2xl hover:bg-zinc-700 text-zinc-700 font-semibold hover:text-white py-2 px-4 border border-zinc-700 hover:border-transparent rounded">Start</button></div>
+      </div>
     </div>
   )
 }
