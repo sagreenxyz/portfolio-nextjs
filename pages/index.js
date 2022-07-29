b3d640473c61fc6b6beefd682e137f80f75ef241
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-28 20:25:48 -0500

    adjusting text size on breakpoints

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Home() {
     <>
       <div className="h-screen grid grid-cols-1">
         <div className="place-self-center">
-          <div className="text-center text-3xl py-6">Introducing...</div>
-          <div className="text-center text-5xl md:text-7xl lg:text-8xl font-bold text-zinc-600">SAGreenXYZ</div>
+          <div className="px-14 text-xl md:text-3xl py-6">Introducing...</div>
+          <div className="text-center text-4xl md:text-6xl lg:text-8xl font-bold text-zinc-600">SAGreenXYZ</div>
           <div className="text-center py-6">a.k.a. Scott A. Green</div>
           <div className="text-center py-10">
             <button onClick={() => setShow(true)} className="shadow-2xl hover:bg-zinc-700 text-zinc-700 font-semibold hover:text-white py-2 px-4 border border-zinc-700 hover:border-transparent rounded">
               Start
             </button>
-            <div className={show ? "visible py-6 text-5xl text-orange-600" : "invisible py-6 text-5xl"}>
+            <div className={show ? "visible py-6 text-xl md:text-5xl text-orange-600" : "invisible py-6 text-5xl"}>
               Not Yet!...  We&apos;re not quite ready.
             </div>
           </div>
